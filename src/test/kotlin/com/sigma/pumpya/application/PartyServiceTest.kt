a771b723e196ccf226c2d49d2ac7baa217381e3d
seonghyeoncho <chosh040@naver.com>
2024-05-28 21:11:22 +0900

    fix test for build

--- src/test/kotlin/com/sigma/pumpya/application/PartyServiceTest.kt
+++ src/test/kotlin/com/sigma/pumpya/application/PartyServiceTest.kt
@@ -6,13 +6,4 @@ import org.springframework.data.redis.core.RedisTemplate
 
 class PartyServiceTest: BehaviorSpec({
 
-    val redisTemplate  = mockk<RedisTemplate<String, String>>()
-    val redisPublisherService = mockk<RedisPublisherService>()
-    val partyService = PartyService(redisTemplate, redisPublisherService)
-
-    afterTest {
-
-    }
-    given("create party test") {
-    }
 })

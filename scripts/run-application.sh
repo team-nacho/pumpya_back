daa6c3adfeec6cf0398583fb77f4d56ebedab838
seonghyeoncho <chosh040@naver.com>
2024-05-29 23:31:22 +0900

    환경 변수 로직 테스트: 2

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -8,5 +8,22 @@ ERROR_LOG="$LOG_DIR/app-error.log"
 # Ensure log directory exists
 mkdir -p $LOG_DIR
 
+REDIS_CLOUD_HOST=$(aws ssm get-parameter --name "REDIS_CLOUD_HOST" --with-decryption --query "Parameter.Value" --output text)
+REDIS_CLOUD_PORT=$(aws ssm get-parameter --name "REDIS_CLOUD_PORT" --with-decryption --query "Parameter.Value" --output text)
+REDIS_CLOUD_PASSWORD=$(aws ssm get-parameter --name "REDIS_CLOUD_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
+REDIS_CLOUD_NAME=$(aws ssm get-parameter --name "REDIS_CLOUD_NAME" --with-decryption --query "Parameter.Value" --output text)
+AWS_RDS_URL=$(aws ssm get-parameter --name "RDS_URL" --with-decryption --query "Parameter.Value" --output text)
+AWS_RDS_NAME=$(aws ssm get-parameter --name "RDS_NAME" --with-decryption --query "Parameter.Value" --output text)
+AWS_RDS_PASSWORD=$(aws ssm get-parameter --name "RDS_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
+
+export REDIS_CLOUD_HOST
+export REDIS_CLOUD_PORT
+export REDIS_CLOUD_PASSWORD
+export REDIS_CLOUD_NAME
+export AWS_RDS_URL
+export AWS_RDS_NAME
+export AWS_RDS_PASSWORD
+
+
 # Start Spring Boot application
 nohup java -jar /home/ec2-user/pump/build/libs/pumpya-0.0.1-SNAPSHOT.jar >> $APP_LOG 2>> $ERROR_LOG &

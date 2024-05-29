d7874c8049cd2e0a624353d9f34584b8558a7065
seonghyeoncho <chosh040@naver.com>
2024-05-29 15:33:46 +0900

    change change-script-permissions.sh

--- scripts/change-script-permissions.sh
+++ scripts/change-script-permissions.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-chmod +x /home/ec2-user/change-script-permissions.sh
-chmod +x /home/ec2-user/delete-before-artifacts.sh
-chmod +x /home/ec2-user/run-application.sh
+chmod +x /home/ec2-user/pump
+chmod +x /home/ec2-user/pump/*/**

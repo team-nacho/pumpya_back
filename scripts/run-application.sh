83f7c23d655f81d7d0fcf0aca290f55803a1c7bc
seonghyeoncho <chosh040@naver.com>
2024-06-02 05:39:49 +0900

    무중단 테스트 3 권한 수정, 실행 파일 수정

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -21,4 +21,4 @@ AWS_RDS_PASSWORD=$(aws ssm get-parameter --name "RDS_PASSWORD" --with-decryption
 export REDIS_CLOUD_HOST REDIS_CLOUD_PORT REDIS_CLOUD_PASSWORD REDIS_CLOUD_NAME AWS_RDS_URL AWS_RDS_NAME AWS_RDS_PASSWORD
 
 # Start Spring Boot application
-nohup java -jar /home/ec2-user/pump/build/libs/pumpya-0.0.1-SNAPSHOT.jar >> $APP_LOG 2>> $ERROR_LOG &
+nohup java -jar /home/ec2-user/pump/build/libs/pump.jar >> $APP_LOG 2>> $ERROR_LOG &

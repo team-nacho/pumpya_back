c5bb1faf9441f2b786975cd1fe5f1d3d5a02586b
seonghyeoncho <chosh040@naver.com>
2024-06-01 23:55:00 +0900

    delete-before-artifacts.sh 수정, run-application.sh 수정

--- scripts/delete-before-artifacts.sh
+++ scripts/delete-before-artifacts.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Check if the application is running and stop it
-if pgrep -f "java" > /dev/null; then
-    sudo systemctl stop java
+
+# PID 변수 생성
+PID=$(pgrep -f "java")
+
+if [ -n "$PID" ]; then
+  kill $PID
+  sleep 5
+
+  if ps -p $PID > /dev/null; then
+    kill -9 $PID
 fi
 
 # Remove the old artifact

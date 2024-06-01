45ee7e6f7e4482162b9fc8fee66fb84a0fa6a987
seonghyeoncho <chosh040@naver.com>
2024-06-02 05:23:05 +0900

    무중단 테스트 2

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Log directories
-
+BASE_PATH=/home/ec2-user/pump
+LOG_DIR="$BASE_PATH/log"
+APP_LOG="$LOG_DIR/app.log"
+ERROR_LOG="$LOG_DIR/app-error.log"
 
 # Ensure log directory exists
 mkdir -p $LOG_DIR
@@ -18,4 +21,4 @@ AWS_RDS_PASSWORD=$(aws ssm get-parameter --name "RDS_PASSWORD" --with-decryption
 export REDIS_CLOUD_HOST REDIS_CLOUD_PORT REDIS_CLOUD_PASSWORD REDIS_CLOUD_NAME AWS_RDS_URL AWS_RDS_NAME AWS_RDS_PASSWORD
 
 # Start Spring Boot application
-nohup java -jar /home/ec2-user/pump/build/libs/pumpya-0.0.1-SNAPSHOT.jar
+nohup java -jar /home/ec2-user/pump/build/libs/pumpya-0.0.1-SNAPSHOT.jar >> $APP_LOG 2>> $ERROR_LOG &
